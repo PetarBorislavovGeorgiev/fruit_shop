c76ca22f27a0a42afd7f54f210e7a75f14950054
Petar Georgiev <petar.b.georgiev@gmail.com>
2024-07-22 01:37:07 +0300

    Creating the project

---
+++ src/test/java/bg/softuni/fruit_shop/FruitShopApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.fruit_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
