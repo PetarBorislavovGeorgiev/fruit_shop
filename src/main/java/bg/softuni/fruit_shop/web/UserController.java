a2d975459d1165e4953123ebb88eca4e33181b19
Petar Georgiev <petar.b.georgiev@gmail.com>
2024-08-09 05:57:02 +0300

    Add I18N

--- src/main/java/bg/softuni/fruit_shop/web/UserController.java
+++ src/main/java/bg/softuni/fruit_shop/web/UserController.java
@@ -1,6 +1,9 @@
 package bg.softuni.fruit_shop.web;
 
+import bg.softuni.fruit_shop.service.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -17,5 +20,12 @@ public class UserController {
     @GetMapping("/contact")
     public String contact() {
         return "contact";
+    }
+
+    @GetMapping("/users/details")
+    public String getUserDetails(@AuthenticationPrincipal CustomUserDetails customUserDetails,
+                                 Model model) {
+
+        return "user-details";
     }
 }
